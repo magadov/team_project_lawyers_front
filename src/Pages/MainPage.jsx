65c6f290e677d575923ed7f86ce7742632687336
Vis <awwya@mail.ru>
2022-02-16 14:36:45 +0300

    First commit

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../components/Header';
+import css from './MainPage.module.css'
+
+const MainPage = () => {
+  return (
+    <div className={css.PageImage}>
+      <Header />
+      <div className={css.content}>
+        <h1> Личный юрист </h1>
+        <p>Онлайн консультация по любым юридическим вопросам</p>
+        <div className={css.desc}>
+        <div className={css.call}>
+          <h3>24/7</h3>
+          <p>Звоните в<br /> любое время</p>
+        </div>
+        <div className={css.des}>
+          <h3>Более 170</h3>
+          <p>Опытных юристов</p>
+        </div>
+          <div className={css.free}>
+            <h3>Первая консультация</h3>
+            <p>Абсолютно бесплатно!</p>
+          </div>
+        </div>
+        <button type="button" className="btn btn-info btn-lg">Оставить заявку</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/components/Lawyer/Lawyer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lawyer = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Lawyer;
