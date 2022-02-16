6492448a5af33ffa81a3e8db61e827e4be783f00
Али Умаров <ali_umarov@Alis-MacBook-Pro.local>
2022-02-16 15:13:08 +0300

    release 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from './components/Header';
 import MainPage from './Pages/MainPage';
 // import { Routes, Route } from 'react-router-dom';
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { applyMiddleware, combineReducers, createStore } from 'redux';
+// import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-export  const store = createStore(
-  combineReducers({
-  }),
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// export  const store = createStore(
+//   combineReducers({
+//   }),
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
