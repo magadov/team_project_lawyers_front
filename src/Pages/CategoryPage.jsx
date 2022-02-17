3506d77e82f7d2fcc860b37adac6c5bcae6e3257
usman <dzakaev.94@mail.ru>
2022-02-17 15:33:43 +0300

    категория исправлена

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Category from '../components/Categories';
+import Header from '../components/Header';
+import css from '../components/Header/header.module.css'
 
 const CategoryPage = () => {
   return (
-    <div>
+    <div className={css.PageImage}>
+      <Header/>
       <Category/>
     </div>
   );
